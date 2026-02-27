7d3913e4f9f825c6e4feb7168bc2d91d19b4e93f
Sami932 <jongwoni0516@gmail.com>
2026-02-27 12:56:22 -0500

    add halfPyramid.cpp

---
+++ basic/cpp-beginner-level/3-patterns/halfPyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int height;
+    cout << "Enter the height of the pyramid: ";
+    cin >> height;
+    for (int i = 1; i <= height; i++){
+        for (int j = 1; j <=i; j++){
+            if (j % 2 == 0)
+                cout << "A";
+            else
+                cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
