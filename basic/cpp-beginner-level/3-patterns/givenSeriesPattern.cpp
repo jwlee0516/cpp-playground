669c628b2869170e1dc3a691d2cbb61e8d1fa75b
Sami932 <jongwoni0516@gmail.com>
2026-02-27 13:04:59 -0500

    add givenSeriesPattern.cpp

---
+++ basic/cpp-beginner-level/3-patterns/givenSeriesPattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long long reverse(long long num, long long rev_num = 0){
+    while (num > 0){
+        rev_num = (num % 10) + rev_num * 10;
+        num = num / 10;
+    }
+    return rev_num / 10;
+}
+
+int main(){
+    int long long num, sum = 0;
+
+    cout << "\nEnter the first number of this pattern : ";
+    cin >> num;
+
+    while (num > 0){
+        cout << num << ", ";
+        num = reverse(num);
+    }
+
+    return 0;
+}
