cc9d44a2102259e1ed87f32777430dec5c0a58b5
Sami932 <jongwoni0516@gmail.com>
2026-02-27 20:13:06 -0500

    add findGCDLCM.cpp

---
+++ basic/cpp-beginner-level/4-Numbers/findGCDLCM.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// function implemented using Euclidean theorem
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int num1, num2, GCD, LCM;
+    cout << "Enter the number1: ";
+    cin >> num1;
+    cout << "Enter the number2: ";
+    cin >> num2;
+
+    GCD = gcd(num1, num2);
+    LCM = num1 * num2 / GCD;
+
+    cout << "GCD: " << GCD << endl;
+    cout << "LCM: " << LCM << endl;
+
+    return 0;
+}
