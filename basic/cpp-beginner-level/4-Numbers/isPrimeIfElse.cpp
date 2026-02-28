8add15c4867536308e7ab71ffc06cbfc568fc46f
Sami932 <jongwoni0516@gmail.com>
2026-02-27 19:06:43 -0500

    addisPrimeIfElse.cpp

---
+++ basic/cpp-beginner-level/4-Numbers/isPrimeIfElse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool checkPrime(int num)
+{
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter the number to check Prime or Not: ";
+    cin >> num;
+
+    if(checkPrime(num))
+        cout << num << " is a Prime number";
+    else
+        cout << num << " is not a Prime number";
+  return 0;
+}
