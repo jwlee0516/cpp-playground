134cf43a7666b6fca390f04ca3767843dd006ff4
Sami932 <jongwoni0516@gmail.com>
2026-02-28 13:33:01 -0500

    add isArrayPrime.cpp

---
+++ basic/cpp-beginner-level/5-arrays/isArrayPrime.cpp
@@ -0,0 +1,29 @@
+#include "bits-stdc++.h"
+using namespace std;
+
+
+int checkForPrime(int num)
+{
+    if (num <= 1) return 0;
+
+    for (int j = 2; j <= sqrt(num); j++)
+    {
+        if (num % j == 0) return 0;
+    }
+
+    return 1;
+}
+int main()
+{
+    int arr[] = {1, 3, 5, 4, 8, 13, 11};
+    int n = 7;
+    int count = 0;
+    int isElementPrime = 0;
+    for (int i = 0; i < n; i++)
+    {
+        isElementPrime = checkForPrime(arr[i]);
+        if (isElementPrime == 1) count++;
+    }
+    cout << "Count of number of primes in array : " << count << endl;
+    return 0;
+}
