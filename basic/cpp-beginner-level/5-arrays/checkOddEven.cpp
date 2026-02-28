8080eed92b44f643d607285553add76d11293c63
Sami932 <jongwoni0516@gmail.com>
2026-02-28 13:53:17 -0500

    add checkOddEven.cpp

---
+++ basic/cpp-beginner-level/5-arrays/checkOddEven.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+string isEven(int n)
+{
+  if(n % 2 == 0)
+    return " is Even";
+  return " is Odd";
+}
+
+int main()
+{
+  int size;
+  cout << "Enter the size of the array:";
+  cin >> size;
+  int arr[size];
+
+  cout << "Enter the elements of the array:";
+  for(int i = 0; i < size; i++)
+  {
+    cin >> arr[i];
+  }
+
+  for(int i = 0; i < size; i++)
+  {
+    cout << arr[i] << isEven(arr[i]) << "\n";
+  }
+  return 0;
+
+}
