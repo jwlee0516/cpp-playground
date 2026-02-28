f3cf81b1ac4b0c0ece347674e3918622c30e7f24
Sami932 <jongwoni0516@gmail.com>
2026-02-27 20:22:49 -0500

    add findNumReverse.cpp

---
+++ basic/cpp-beginner-level/4-Numbers/findNumReverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num, reverse = 0;
+  cout << "Enter the number: ";
+  cin >> num;
+
+  while(num > 0)
+  {
+    reverse = reverse * 10 + (num % 10);
+    num /= 10;
+  }
+  cout << "reverse: " << reverse << endl;
+  return 0;
+}
