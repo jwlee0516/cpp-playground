a127bbcfd36c4fd25b9c8aa8930be575cfc205c4
Sami932 <jongwoni0516@gmail.com>
2026-02-27 19:39:17 -0500

    add EvenOddIfElse.cpp

---
+++ basic/cpp-beginner-level/4-Numbers/EvenOddIfElse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+  
+    cout << "Enter the number to chech for Even/odd: ";
+    cin >> num;
+
+    if(num % 2 == 0 ) 
+        cout << num << " is even" << endl;
+    else
+        cout << num << " is odd" << endl;
+  
+    return 0;
+}
