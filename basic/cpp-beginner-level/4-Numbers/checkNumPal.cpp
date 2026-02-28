08965c6b4be454f2973b9bcf29d715c88a74d10c
Sami932 <jongwoni0516@gmail.com>
2026-02-27 19:30:06 -0500

    add checkNumPal.cpp

---
+++ basic/cpp-beginner-level/4-Numbers/checkNumPal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int num)
+{
+    int temp = num;
+    int reverseNum = 0;
+    while (temp > 0)
+    {
+        reverseNum = reverseNum * 10 + temp  % 10;
+        temp = temp / 10;
+    }
+    if (reverseNum == num)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter the number to check for Palindrome: ";
+    cin >> num;
+
+    if (isPalindrome(num))
+        cout << num << " is Palindrome number.";
+    else
+        cout << num << " is not a Palindrome number.";
+
+    return 0;
+}
