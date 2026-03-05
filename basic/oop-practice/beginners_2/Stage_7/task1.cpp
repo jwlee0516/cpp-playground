9e1e0da2c24398630c047127ada251cf76db4574
Sami932 <jongwoni0516@gmail.com>
2026-03-04 21:05:30 -0500

    update ComplexNumber class

--- basic/oop-practice/beginners_2/Stage_7/task1.cpp
+++ basic/oop-practice/beginners_2/Stage_7/task1.cpp
@@ -37,5 +37,75 @@ Static data member and Static function
 using namespace std;
 
 class ComplexNumber {
+    private:
+        int real;
+        int imaginary;
+        static int count; //will count the total number of objects created
     
+    public:
+        ComplexNumber(int real = 0, int imaginary = 0); // default arguments
+        ComplexNumber(const ComplexNumber& other); // copy constructor
+        ~ComplexNumber();
+
+        void Input();
+        void Output();
+
+        // return the total number of objects created by incrementing as the object is created
+        static int countDisplay();
+        bool isEqual(const ComplexNumber& other);
+        ComplexNumber Conjugate();
+
+        // Adding two complex numbers (a + bi) and (c + di) yields ((a+b) + (c+d)i)
+        ComplexNumber operator+ (const ComplexNumber& num);
+
+        // Subtracting two complex numbers (a + bi) and (c + di) yields ((a-b) + (c-d)i).
+        ComplexNumber operator- (const ComplexNumber& num);
+
+        // Multiplying two complex numbers (a + bi) and (c + di) yields ((ac-bd) + (ad+bc)i).
+        ComplexNumber operator* (const ComplexNumber& num);
+
+        // Increment and decrement operators should only add 1 or subtract 1 from the real part
+        ComplexNumber& operator++(); // pre-increment
+        ComplexNumber& operator--() // pre-decrement
+        ComplexNumber& operator++(int) // post-increment
+        ComplexNumber operator--(int); // post-decrement
+
+        bool operator>=(const ComplexNumber& num)
+        bool operator<=(const ComplexNumber& num);
+        bool operator!=(const ComplexNumber& num);
+        bool operator==(const ComplexNumber& num);
+};
+
+int ComplexNumber::count = 0;
+
+ComplexNumber::ComplexNumber(int real, int imaginary) {
+    this->real = real;
+    this->imaginary = imaginary;
+    count++;
 }
+
+ComplexNumber::ComplexNumber(const ComplexNumber& other) {
+    this->real = other.real;
+    this->imaginary = other.imaginary;
+    count++;
+}
+
+ComplexNumber::~ComplexNumber(){}
+
+void ComplexNumber::Input() {
+    cout << "Enter real part: ";
+    cin >> this->real;
+    cout << "Enter imaginary part: ";
+    cin >> this->imaginary;
+}
+
+void ComplexNumber::Output() {
+    cout << "Answer: ";
+    cout << this->real << "+";
+    cout << "(" << this->imaginary << "i)";
+}
+
+int ComplexNumber::countDisplay() {
+    return count;
+}
+
