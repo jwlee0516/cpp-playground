1c2fee30c7c2ec00a5d99ad4681a352a1101e0a1
Sami932 <jongwoni0516@gmail.com>
2026-03-04 21:15:04 -0500

    this pointer, operater overloading (binary & unary), operator overloading as member functions, non-member, and friend functions, static data members

--- basic/oop-practice/beginners_2/Stage_7/task1.cpp
+++ basic/oop-practice/beginners_2/Stage_7/task1.cpp
@@ -109,3 +109,111 @@ int ComplexNumber::countDisplay() {
     return count;
 }
 
+bool ComplexNumber::isEqual(const ComplexNumber& other) {
+    return (this->real == other.real && this->imaginary == other.imaginary);
+}
+
+ComplexNumber ComplexNumber::Conjugate() {
+    ComplexNumber c;
+    c.real = this->real;
+    c.imaginary = this->imaginary;
+    return c;
+}
+
+ComplexNumber ComplexNumber::operator+(const ComplexNumber& num) {
+    ComplexNumber c;
+    c.real = this->real + num.real;
+    c.imaginary = this->imaginary + num.imaginary;
+    return c;
+}
+
+ComplexNumber ComplexNumber::operator-(const ComplexNumber& num) {
+    ComplexNumber c;
+    c.real = this->real - num.real;
+    c.imaginary = this->imaginary - num.imaginary;
+    return c;
+}
+
+// Multiplying two complex numbers (a + bi) and (c + di) yields ((ac-bd) + (ad+bc)i).
+ComplexNumber ComplexNumber::operator*(const ComplexNumber& num) {
+    ComplexNumber c;
+    c.real = this->real * num.real - this->imaginary * num.imaginary;
+    c.imaginary = this->real * num.imaginary + this->imaginary * num.real;
+    return c;
+}
+
+ComplexNumber& ComplexNumber::operator++() {
+    this->real++;
+    return *this;
+}
+
+ComplexNumber& ComplexNumber::operator--() {
+    this->real--;
+    return *this;
+}
+
+ComplexNumber ComplexNumber::operator++(int) {
+    ComplexNumber c = *this;
+    this->real++;
+    return c;
+}
+
+ComplexNumber ComplexNumber::operator--(int) {
+    ComplexNumber c = *this;
+    this->real--;
+    return c;
+}
+
+bool ComplexNumber::operator>=(const ComplexNumber& num) {
+    return (this->real >= num.real && this->imaginary >= num.imaginary);
+}
+
+bool ComplexNumber::operator<=(const ComplexNumber& num) {
+    return (this->real <= num.real && this->imaginary <= num.imaginary);
+}
+bool ComplexNumber::operator==(const ComplexNumber& num) {
+    return (this->real == num.real && this->imaginary == num.imaginary);
+}
+bool ComplexNumber::operator!=(const ComplexNumber& num) {
+    return !(*this == num);
+}
+
+int main() {
+    ComplexNumber c1(1, 3), c2(3, 7), c3, c4, c5;
+    
+    cout << "Sum of C1 and C2 is: ";
+    c3 = c1 + c2;
+    c3.Output();
+    cout << endl;
+
+    cout << "Subtraction of C1 and C2 is: "; 
+    c4 = c1 - c2;
+    c4.Output();
+    cout << endl;
+
+    cout << "Multiplication of C1 and C2 is: "; 
+    c5 = c1 * c2;
+    c5.Output();
+    cout << endl;
+
+    cout << c1.isEqual(c2) << endl;
+    cout << endl;
+
+    cout << "Increment of C1: "; 
+    c1++; 
+    ComplexNumber c6 = c1; 
+    c6.Output(); 
+    cout << endl;
+
+    cout << "Decrement of C1: "; 
+    c1--; 
+    ComplexNumber c7 = c1; 
+    c7.Output(); 
+    cout << endl;
+
+    c4.Input();
+    c4.Output();
+
+    return 0; 
+}
+
