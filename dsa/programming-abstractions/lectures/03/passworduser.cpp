40a2e988aa4dbd236ca113e9ddcf64c2e9fdbb81
Sami932 <jongwoni0516@gmail.com>
2026-04-17 19:29:01 -0400

    check whether the username appears in the password string if we extract just the alphabetic portion

--- dsa/programming-abstractions/lectures/03/passworduser.cpp
+++ dsa/programming-abstractions/lectures/03/passworduser.cpp
@@ -6,7 +6,16 @@ using namespace std;
 int main()
 {
     string s = "Sean";
-    string p = "sean'spassword";
+    string p = "s1e1a1n1'1s1p1a1s1s1w1o1r1d";
+    string p0;
+
+    for (int i = 0; i < p.length(); i++)
+    {
+        if(isalpha(p[i]))
+        {
+            p0 += p[i];
+        }
+    }
 
     // How can we determine whether the given username (s) appears in the password (p)?
     if(stringContains(toLowerCase(p), toLowerCase(s)))
